7511f8d9b5a83f8875192077b41ec2645b831abd
samitkapoor <samitkapoor77@gmail.com>
2022-05-28 19:59:03 +0530

    Better naming

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:tictactoe/views/game_screen.dart';
+import 'package:tictactoe/core/game_screen.dart';
 
 void main() {
   runApp(Tictactoe());
